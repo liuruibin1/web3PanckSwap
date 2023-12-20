14cc4dc11d375f70419c07150b0ad493c49ea66b
Administrator <langzibingxiu@gmail.com>
2023-12-20 10:20:47 +0800

    dev 3

--- src/main/java/com/panck/swap/abi/Panckswap.java
+++ src/main/java/com/panck/swap/abi/Panckswap.java
@@ -1,5 +1,10 @@
 package com.panck.swap.abi;
 
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
 import org.web3j.abi.TypeReference;
 import org.web3j.abi.datatypes.Address;
 import org.web3j.abi.datatypes.DynamicArray;
@@ -13,12 +18,6 @@ import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.tx.Contract;
 import org.web3j.tx.TransactionManager;
 import org.web3j.tx.gas.ContractGasProvider;
-
-import java.math.BigInteger;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.Callable;
 
 /**
  * <p>Auto generated code.
